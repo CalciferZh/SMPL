991de0a187e8b44ccdfa634d462f616768e986ff
Calcifer <calciferzh@outlook.com>
2018-03-17 09:31:27 +0800

    Revert "support using rotation matrix in smpl_tf directly"
    This reverts commit 98777b7636730caac46d972b432820beac4d1519, since
    using R directly is foolish.

--- smpl_tf.py
+++ smpl_tf.py
@@ -30,15 +30,7 @@ def pack(x):
     return tf.concat((tf.zeros((x.get_shape().as_list()[0], 4, 3), dtype=tf.float32), x), axis=2)
 
 
-def smpl_model(model_path, trans, betas, pose=None, R=None):
-    if R is not None:
-        R_cube_big = R
-    elif pose is not None:
-        R_cube_big = rodrigues(tf.reshape(pose, (-1, 1, 3)))
-    else:
-        print('Error: pose and R can not be both None.')
-        return None
-    
+def smpl_model(model_path, betas, pose, trans):
     with open(model_path, 'rb') as f:
         params = pickle.load(f)
 
@@ -48,6 +40,7 @@ def smpl_model(model_path, trans, betas, pose=None, R=None):
     v_template = tf.constant(params['v_template'], dtype=np.float32)
     shapedirs = tf.constant(params['shapedirs'], dtype=np.float32)
     f = params['f']
+
     kintree_table = params['kintree_table']
     id_to_col = {kintree_table[1, i]: i for i in range(kintree_table.shape[1])}
     parent = {
@@ -56,10 +49,13 @@ def smpl_model(model_path, trans, betas, pose=None, R=None):
     }
     v_shaped = tf.tensordot(shapedirs, betas, axes=[[2], [0]]) + v_template
     J = tf.matmul(J_regressor, v_shaped)
+    pose_cube = tf.reshape(pose, (-1, 1, 3))
+    R_cube_big = rodrigues(pose_cube)
     R_cube = R_cube_big[1:]
     I_cube = tf.expand_dims(tf.eye(3, dtype=tf.float32), axis=0) + tf.zeros((R_cube.get_shape()[0], 3, 3), dtype=tf.float32)
     lrotmin = tf.squeeze(tf.reshape((R_cube - I_cube), (-1, 1)))
     v_posed = v_shaped + tf.tensordot(posedirs, lrotmin, axes=[[2], [0]])
+    pose = tf.reshape(pose, (-1, 3))
     results = []
     results.append(with_zeros(tf.concat((R_cube_big[0], tf.reshape(J[0, :], (3, 1))), axis=1)))
     for i in range(1, kintree_table.shape[1]):
@@ -87,7 +83,7 @@ if __name__ == '__main__':
     betas = tf.constant(betas, dtype=tf.float32)
     trans = tf.constant(trans, dtype=tf.float32)
 
-    output, faces = smpl_model('./model.pkl', trans, betas, pose)
+    output, faces = smpl_model('./model.pkl', betas, pose, trans)
     sess = tf.Session()
     result = sess.run(output)
 
