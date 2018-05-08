9e5a6cc0a564692e53ff23bbaf6068276e70f95c
Calcifer <calciferzh@outlook.com>
2018-05-08 20:08:11 +0800

    update CMU MoCap Marker

--- CMU_Mocap_Markers.pp
+++ CMU_Mocap_Markers.pp
@@ -1,51 +1,50 @@
 <!DOCTYPE PickedPoints>
 <PickedPoints>
  <DocumentData>
-  <DateTime time="15:42:16" date="2018-04-20"/>
+  <DateTime time="09:17:35" date="2018-05-05"/>
   <User name="Calcifer"/>
   <DataFileName name="smpl_std.obj"/>
   <templateName name=""/>
  </DocumentData>
- <point z="0.106202" active="1" y="-0.249063" x="0.121396" name="LFWT"/>
- <point z="0.0154939" active="1" y="-0.562686" x="0.157391" name="LTHI"/>
- <point z="-0.0150601" active="1" y="0.232609" x="0.700323" name="LWRA"/>
- <point z="-0.0852711" active="1" y="-0.158404" x="0.0659384" name="LBWT"/>
- <point z="-0.085239" active="1" y="-0.150201" x="-0.0731126" name="RBWT"/>
- <point z="-0.0830027" active="1" y="0.227918" x="-0.562737" name="RFRM"/>
- <point z="-0.0774447" active="1" y="0.224629" x="0.638789" name="LFRM"/>
- <point z="-0.0144781" active="1" y="0.227796" x="-0.69987" name="RWRA"/>
- <point z="-0.0509818" active="1" y="0.242655" x="0.807945" name="LFIN"/>
- <point z="-0.0450808" active="1" y="0.242155" x="-0.803738" name="RFIN"/>
- <point z="0.128859" active="1" y="0.0782484" x="0.00363971" name="STRN"/>
- <point z="-0.0704796" active="1" y="0.240187" x="0.477" name="LELB"/>
- <point z="-0.0745501" active="1" y="0.243352" x="-0.486946" name="RELB"/>
- <point z="0.0175551" active="1" y="-0.717519" x="0.143684" name="LKNE"/>
- <point z="0.00482713" active="1" y="-0.72311" x="-0.149229" name="RKNE"/>
- <point z="-0.0449793" active="1" y="0.259102" x="0.38381" name="LUPA"/>
- <point z="-0.0225696" active="1" y="-0.939813" x="0.149628" name="LSHN"/>
- <point z="-0.0256784" active="1" y="-0.823115" x="-0.164324" name="RSHN"/>
- <point z="-0.0119614" active="1" y="0.289627" x="-0.181264" name="RSHO"/>
- <point z="-0.0514458" active="1" y="0.260493" x="-0.296973" name="RUPA"/>
- <point z="-0.022693" active="1" y="0.291166" x="0.180067" name="LSHO"/>
- <point z="-0.0472637" active="1" y="0.46868" x="-0.066234" name="RBHD"/>
- <point z="0.0682239" active="1" y="0.499457" x="0.0655544" name="LFHD"/>
- <point z="-0.0289778" active="1" y="-1.1146" x="0.125006" name="LANK"/>
- <point z="-0.0354472" active="1" y="-1.11838" x="-0.129876" name="RANK"/>
- <point z="-0.087829" active="1" y="-1.17872" x="-0.100619" name="RHEE"/>
- <point z="0.155405" active="1" y="-1.19548" x="-0.0818829" name="RTOE"/>
- <point z="0.156651" active="1" y="-1.19346" x="0.0761749" name="LTOE"/>
- <point z="0.115593" active="1" y="-1.19045" x="0.151526" name="LMT5"/>
- <point z="0.114638" active="1" y="-1.1934" x="-0.158087" name="RMT5"/>
- <point z="-0.0749998" active="1" y="0.229763" x="-0.694048" name="RWRB"/>
- <point z="-0.0716145" active="1" y="0.228968" x="0.697859" name="LWRB"/>
- <point z="0.0328069" active="1" y="-0.453177" x="-0.181637" name="RTHI"/>
- <point z="0.103579" active="1" y="-0.24681" x="-0.124175" name="RFWT"/>
- <point z="-0.112404" active="1" y="0.105276" x="-0.000884878" name="T8"/>
- <point z="-0.116617" active="1" y="0.200451" x="-0.0548183" name="RBAC"/>
- <point z="-0.0943715" active="1" y="0.0112362" x="0.00133582" name="T10"/>
- <point z="0.0642794" active="1" y="0.279804" x="0.00377174" name="CLAV"/>
- <point z="-0.0501358" active="1" y="0.335392" x="-0.00135936" name="C7"/>
- <point z="-0.0395846" active="1" y="0.470011" x="0.0744193" name="LBHD"/>
- <point z="0.0693675" active="1" y="0.498245" x="-0.0602648" name="RFHD"/>
- <point z="-0.0842567" active="1" y="-1.18064" x="0.0986707" name="LHEE"/>
+ <point x="0.121396" y="-0.249063" z="0.106202" active="1" name="LFWT"/>
+ <point x="0.0659384" y="-0.158404" z="-0.0852711" active="1" name="LBWT"/>
+ <point x="-0.0731126" y="-0.150201" z="-0.085239" active="1" name="RBWT"/>
+ <point x="-0.69987" y="0.227796" z="-0.0144781" active="1" name="RWRA"/>
+ <point x="0.807945" y="0.242655" z="-0.0509818" active="1" name="LFIN"/>
+ <point x="-0.803738" y="0.242155" z="-0.0450808" active="1" name="RFIN"/>
+ <point x="0.00363971" y="0.0782484" z="0.128859" active="1" name="STRN"/>
+ <point x="0.143684" y="-0.717519" z="0.0175551" active="1" name="LKNE"/>
+ <point x="0.38381" y="0.259102" z="-0.0449793" active="1" name="LUPA"/>
+ <point x="-0.181264" y="0.289627" z="-0.0119614" active="1" name="RSHO"/>
+ <point x="-0.296973" y="0.260493" z="-0.0514458" active="1" name="RUPA"/>
+ <point x="0.180067" y="0.291166" z="-0.022693" active="1" name="LSHO"/>
+ <point x="-0.066234" y="0.46868" z="-0.0472637" active="1" name="RBHD"/>
+ <point x="0.0655544" y="0.499457" z="0.0682239" active="1" name="LFHD"/>
+ <point x="0.125006" y="-1.1146" z="-0.0289778" active="1" name="LANK"/>
+ <point x="-0.129876" y="-1.11838" z="-0.0354472" active="1" name="RANK"/>
+ <point x="-0.100619" y="-1.17872" z="-0.087829" active="1" name="RHEE"/>
+ <point x="-0.0818829" y="-1.19548" z="0.155405" active="1" name="RTOE"/>
+ <point x="0.0761749" y="-1.19346" z="0.156651" active="1" name="LTOE"/>
+ <point x="0.151526" y="-1.19045" z="0.115593" active="1" name="LMT5"/>
+ <point x="-0.158087" y="-1.1934" z="0.114638" active="1" name="RMT5"/>
+ <point x="-0.124175" y="-0.24681" z="0.103579" active="1" name="RFWT"/>
+ <point x="-0.0548183" y="0.200451" z="-0.116617" active="1" name="RBAC"/>
+ <point x="0.00133582" y="0.0112362" z="-0.0943715" active="1" name="T10"/>
+ <point x="0.00377174" y="0.279804" z="0.0642794" active="1" name="CLAV"/>
+ <point x="-0.00135936" y="0.335392" z="-0.0501358" active="1" name="C7"/>
+ <point x="0.0744193" y="0.470011" z="-0.0395846" active="1" name="LBHD"/>
+ <point x="-0.0602648" y="0.498245" z="0.0693675" active="1" name="RFHD"/>
+ <point x="0.0986707" y="-1.18064" z="-0.0842567" active="1" name="LHEE"/>
+ <point x="-0.168787" y="-0.538486" z="0.0159924" active="1" name="RTHI"/>
+ <point x="-0.146631" y="-0.715237" z="0.0160212" active="1" name="RKNE"/>
+ <point x="0.459408" y="0.244252" z="-0.0650031" active="1" name="LELB"/>
+ <point x="-0.460416" y="0.244725" z="-0.0681055" active="1" name="RELB"/>
+ <point x="0.158765" y="-0.833603" z="-0.0242223" active="1" name="LSHN"/>
+ <point x="0.168064" y="-0.460119" z="-0.0037557" active="1" name="LTHI"/>
+ <point x="-0.155229" y="-0.92028" z="-0.0426064" active="1" name="RSHN"/>
+ <point x="0.575786" y="0.251815" z="-0.0719972" active="1" name="LFRM"/>
+ <point x="-0.610716" y="0.24413" z="-0.0768109" active="1" name="RFRM"/>
+ <point x="-0.695452" y="0.239873" z="-0.0736815" active="1" name="RWRB"/>
+ <point x="0.700985" y="0.23881" z="-0.0163114" active="1" name="LWRA"/>
+ <point x="0.69041" y="0.2251" z="-0.0722696" active="1" name="LWRB"/>
 </PickedPoints>
