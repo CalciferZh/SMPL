215c899cafe3ff4d293a28acb1a13b763f80ad11
Yuxiao Zhou <yuxiao.zhou@outlook.com>
2020-02-10 15:07:01 +0800

    hot fix for J_regressor

--- smpl_torch_batch.py
+++ smpl_torch_batch.py
@@ -14,9 +14,14 @@ class SMPLModel(Module):
     self.J_regressor = torch.from_numpy(
       np.array(params['J_regressor'].todense())
     ).type(torch.float64)
-    self.joint_regressor = torch.from_numpy(
-      np.array(params['joint_regressor'].T.todense())
-    ).type(torch.float64)
+    if 'joint_regressor' in params.keys():
+      self.joint_regressor = torch.from_numpy(
+        np.array(params['joint_regressor'].T.todense())
+      ).type(torch.float64)
+    else:
+      self.joint_regressor = torch.from_numpy(
+        np.array(params['J_regressor'].todense())
+      ).type(torch.float64)
     self.weights = torch.from_numpy(params['weights']).type(torch.float64)
     self.posedirs = torch.from_numpy(params['posedirs']).type(torch.float64)
     self.v_template = torch.from_numpy(params['v_template']).type(torch.float64)
